72eeb1b2452cc8024003d9a99ce9564241abe11e
Adilson <sr.adilson@gmail.com>
2021-09-01 08:57:31 -0300

    Create project Cadastro de Produto

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './assets/css/App.css';
+import CadastroProduto from './components/forms/cadastro-produto';
+
+function App() {
+  return (
+  <>
+  <CadastroProduto/>
+  </>    
+  );
+}
+
+export default App;
